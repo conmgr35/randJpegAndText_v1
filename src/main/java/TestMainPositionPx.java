7c121195c58fd9be69cf35c133b74c6804d0b7b4
conmgr35 <conmgr35@gmail.com>
2018-10-17 17:46:47 +0300

    end work day 17-10-2018

--- src/main/java/TestMainPositionPx.java
+++ src/main/java/TestMainPositionPx.java
@@ -8,44 +8,68 @@ public class TestMainPositionPx {
         CorePosition corePosition = new CorePosition();
 
 
-        int sumWidth = 1000, sumHeight = 1000;
-//        ArrayList<Blockk> blocks = new ArrayList<>();
-        while (sumWidth > 0 && sumWidth > 0) {
-            int randWidth = 0, randHeight = 0;
-            int randInt = JRand.natural().min(1).max(3).gen();
-            switch (randInt) {
+        while (corePosition.isFreeSpace()) {
+            switch (JRand.natural().min(1).max(3).gen()) {
                 case 1:
-                    randHeight = 200;
-                    randWidth = 200;
+                    corePosition.calcPosition(200, 200);
+                    System.out.println(200 + " " + 200);
                     break;
                 case 2:
-                    randHeight = 400;
-                    randWidth = 400;
+                    corePosition.calcPosition(400, 400);
+                    System.out.println(400 + " " + 400);
                     break;
                 case 3:
-                    randHeight = 400;
-                    randWidth = 600;
+                    corePosition.calcPosition(400, 600);
+                    System.out.println(400 + " " + 600);
                     break;
             }
-            if ((((randHeight < sumHeight) && (randWidth < sumWidth)) && ((randHeight < 200) && (randWidth < 200))) || ((sumWidth - randWidth == 0) && (sumHeight - randHeight == 0))) {
-                System.out.println(randHeight+" "+randWidth);
-                sumHeight -= randHeight;
-                sumWidth -= randWidth;
-                switch (randInt) {
-                    case 1:
-                        corePosition.calcPosition(200, 200);
-                        break;
-                    case 2:
-                        corePosition.calcPosition(400, 400);
-                        break;
-                    case 3:
-                        corePosition.calcPosition(400, 600);
-                        break;
-                }
-            }
-
+            corePosition.seeArray();
 
         }
+
+//        int sumWidth = 1000, sumHeight = 1000;
+////        ArrayList<Blockk> blocks = new ArrayList<>();
+//
+//
+//
+//            while (sumWidth > 0 && sumWidth > 0) {
+//                int randWidth = 0, randHeight = 0;
+//                int randInt = JRand.natural().min(1).max(3).gen();
+//                switch (randInt) {
+//                    case 1:
+//                        randHeight = 200;
+//                        randWidth = 200;
+//                        break;
+//                    case 2:
+//                        randHeight = 400;
+//                        randWidth = 400;
+//                        break;
+//                    case 3:
+//                        randHeight = 400;
+//                        randWidth = 600;
+//                        break;
+//                }
+//                if (
+//                        (((randHeight < sumHeight) && (randWidth < sumWidth)) && ((sumHeight - randHeight > 200) && (sumWidth - randWidth > 200)))
+//                                || ((sumWidth - randWidth == 0) && (sumHeight - randHeight == 0))) {
+//                    System.out.println(randHeight + " " + randWidth);
+//                    sumHeight -= randHeight;
+//                    sumWidth -= randWidth;
+//                    switch (randInt) {
+//                        case 1:
+//                            corePosition.calcPosition(200, 200);
+//                            break;
+//                        case 2:
+//                            corePosition.calcPosition(400, 400);
+//                            break;
+//                        case 3:
+//                            corePosition.calcPosition(400, 600);
+//                            break;
+//                    }
+//                }
+//            }
+//
+//
 
 
 //        corePosition.calcPosition(200, 200);
@@ -58,6 +82,7 @@ public class TestMainPositionPx {
 //        corePosition.calcPosition(400, 600);
 //        corePosition.calcPosition(400, 400);
 //        corePosition.calcPosition(200, 200);
+        System.out.println("___________________________________________");
         corePosition.seeArray();
 //        for (Blockk bk : blocks) {
 //            System.out.println(bk.toString());
@@ -118,16 +143,27 @@ class CorePosition {
         return isEnd;
     }
 
+    public boolean isFreeSpace() {
+        for (int i = 0; i < iFreeSpace.length; i++) {
+            for (int j = 0; j < iFreeSpace.length; j++) {
+                if (iFreeSpace[i][j] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     void seeArray() {
         for (int i = 0; i < iFreeSpace.length; i += 100) {
             for (int j = 0; j < iFreeSpace.length; j += 100) {
                 if (iFreeSpace[i][j] == 0) {
-                    System.out.print(" - ");
+                    System.out.print(" \033[0m- ");
                 } else {
-                    System.out.print(" + ");
+                    System.out.print(" \033[0;32m+ ");
                 }
             }
-            System.out.println();
+            System.out.println("\033[0m");
         }
     }
 }
