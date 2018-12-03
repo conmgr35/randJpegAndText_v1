30e45bc2645ce29745595b5017114475ab8c904d
conmgr35 <conmgr35@gmail.com>
2018-12-03 11:12:33 +0200

    03-12-2018

--- src/main/java/TestMainPositionPx.java
+++ src/main/java/TestMainPositionPx.java
@@ -4,6 +4,42 @@ import posInBoard.CorePosition;
 public class TestMainPositionPx {
     public static void main(String[] args) {
 
+//        CorePosition corePosition = new CorePosition();
+//
+//
+//        while (corePosition.isFreeSpace()) {
+//            switch (JRand.natural().min(1).max(3).gen()) {
+//                case 1:
+//                    corePosition.calcPosition(200, 200);
+//                    System.out.println(200 + " " + 200);
+//                    break;
+//                case 2:
+//                    corePosition.calcPosition(400, 400);
+//                    System.out.println(400 + " " + 400);
+//                    break;
+//                case 3:
+//                    corePosition.calcPosition(400, 600);
+//                    System.out.println(400 + " " + 600);
+//                    break;
+//            }
+//            corePosition.seeArray();
+//
+//        }
+//
+//
+//        System.out.println("___________________________________________");
+//        corePosition.seeArray();
+//        System.out.println("___________________________________________");
+//        corePosition.seeBlockArray();
+        TestMainPositionPx testMainPositionPx = new TestMainPositionPx();
+        testMainPositionPx.getCurentMax(10);
+
+    }
+
+    public void getCurentMax(int j) {
+        for (int i = 0; i < j; i++) {
+
+
         CorePosition corePosition = new CorePosition();
 
 
@@ -11,28 +47,27 @@ public class TestMainPositionPx {
             switch (JRand.natural().min(1).max(3).gen()) {
                 case 1:
                     corePosition.calcPosition(200, 200);
-                    System.out.println(200 + " " + 200);
+//                    System.out.println(200 + " " + 200);
                     break;
                 case 2:
                     corePosition.calcPosition(400, 400);
-                    System.out.println(400 + " " + 400);
+//                    System.out.println(400 + " " + 400);
                     break;
                 case 3:
                     corePosition.calcPosition(400, 600);
-                    System.out.println(400 + " " + 600);
+//                    System.out.println(400 + " " + 600);
                     break;
             }
-            corePosition.seeArray();
 
         }
 
 
         System.out.println("___________________________________________");
         corePosition.seeArray();
-        System.out.println("___________________________________________");
-        corePosition.seeBlockArray();
-
     }
+}
+
+
 
 
 }
